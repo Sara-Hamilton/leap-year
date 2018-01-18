617ac06285806be5782ea8ea4fd47ea012cb9150
Maggie Summers and Sara Hamilton <maggie@>
2018-01-18 08:40:01 -0800

    add html, jQuery and first step of business logic

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+//biz logic
+var leapYear = function(year) {
+  return false;
+};
+
+//party logic
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+
+    $("#result").text(result);
+  });
+});
